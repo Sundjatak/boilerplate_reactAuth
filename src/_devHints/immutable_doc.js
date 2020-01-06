e46db123ff59b415cb7cd06ede11514d9aeaaada
SundjataK <leo.kourouma@gmail.com>
2020-01-06 18:03:39 +0100

    HINTS

--- src/_devHints/immutable_doc.js
+++ src/_devHints/immutable_doc.js
@@ -23,8 +23,18 @@ console.log(pets.toJS()); // prints ['cat', 'dog'];
 console.log(finalPets.toJS()); // prints ['cat', 'dog', 'goldfish', 'tortoise'];
 
 // You create an Immutable object and you update it — with Immutable,
-// the initial data structure is not changed. It is immutable. 
+// the initial data structure is not changed. It is immutable.
 const data = fromJS({ name: 'Will' });
 const newNameData = data.set('name', 'Susie');
 console.log(data.get('name')); // prints 'Will'
 console.log(newNameData.get('name')); // prints 'Susie'
+
+// # Tutorials/References
+// https://reactjs.org/tutorial/tutorial.html
+// https://egghead.io/courses/the-beginner-s-guide-to-reactjs
+// https://www.valentinog.com/blog/react-redux-tutorial-beginners/
+// https://medium.com/@stowball/a-dummys-guide-to-redux-and-thunk-in-react-d8904a7005d3
+// https://www.raywenderlich.com/178012/react-native-tutorial-building-android-apps-javascript
+// https://medium.com/@rajaraodv/step-by-step-guide-to-building-react-redux-apps-using-mocks-48ca0f47f9a
+// https://www.gatsbyjs.org/tutorial/part-four/
+// https://redux.js.org/advanced/example-reddit-api
